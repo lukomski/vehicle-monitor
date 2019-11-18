a4c33962de45be9327e41ab9203270bab636044c
Jan Łukomski <jlukomski1@gmail.com>
2019-11-18 17:14:08 +0100

    adding save file after upload, but save empty file

--- license_plate_recognition/plate_reader.py
+++ license_plate_recognition/plate_reader.py
@@ -1,4 +1,6 @@
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 import subprocess
 import re
 
@@ -10,13 +12,18 @@ class PlateReader():
         image.read()
         file_name = "samochod.jpg" # h786poj.jpg
         # save image to file TODO - now use static file
+        path = default_storage.save('file.jpg', ContentFile(image.read()))
+        #print("path = " + path)
+
 
         # use aplr to read licence plate
         bashCommand = "alpr /code/" + file_name
+        #bashCommand = "alpr /code/" + path
+        #print("bashCommand = " + bashCommand)
+
 		
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
-        print(output)
         
         m = re.search('-\s*(.*)\s*\\t\s*confidence', output.decode('utf-8'))
         plate_text = m.group(1)
