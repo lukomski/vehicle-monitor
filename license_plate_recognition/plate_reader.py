b9d15a1ee88da5803706b23b655ee3091a265bf3
MikolajKubek <mikolajkubik1998@gmail.com>
2019-11-18 18:50:00 +0100

    tymczasowe zapisywanie obrazu

--- license_plate_recognition/plate_reader.py
+++ license_plate_recognition/plate_reader.py
@@ -8,24 +8,17 @@ class PlateReader():
 
     @staticmethod
     def read_license_plate(image: InMemoryUploadedFile):
-        # file to bytes
-        image.read()
-        file_name = "samochod.jpg" # h786poj.jpg
-        # save image to file TODO - now use static file
-        path = default_storage.save('file.jpg', ContentFile(image.read()))
-        #print("path = " + path)
+        file_name = 'samochod.jpg'
+        with open(file_name, 'wb') as f:
+            f.write(image.read())
 
-
-        # use aplr to read licence plate
         bashCommand = "alpr /code/" + file_name
-        #bashCommand = "alpr /code/" + path
-        #print("bashCommand = " + bashCommand)
-
 		
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         
         m = re.search('-\s*(.*)\s*\\t\s*confidence', output.decode('utf-8'))
+        print(output.decode('utf-8'))
         plate_text = m.group(1)
 
         return ['1234', 'jeszcze jeden', plate_text]
